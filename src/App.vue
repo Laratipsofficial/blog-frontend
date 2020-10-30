80e9518fbb8ba24c87e99544659c6e5c0a32f8f3
Laratips <aashish201030@gmail.com>
2020-10-30 18:11:30 +0000

    complete footer design

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,18 @@
 
     <router-view />
 
-    <footer>
-      footer
-    </footer>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/layouts/Navbar";
+import Footer from "@/components/layouts/Footer";
 
 export default {
   components: {
     Navbar,
+    Footer,
   },
 };
 </script>
---
+++ src/components/layouts/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="text-center py-6 sm:py-12 bg-black text-white text-base">
+    <div class="container mx-auto px-4 sm:px-0">
+      <span>Copyright &copy;2020 All rights reserved.</span>
+
+      <div>
+        <span>Made with love by</span>
+        <span class="ml-1 font-bold">
+          <span>Lara</span>
+          <span class="text-primary">Tips</span>
+        </span>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  props: {},
+
+  setup() {},
+};
+</script>
